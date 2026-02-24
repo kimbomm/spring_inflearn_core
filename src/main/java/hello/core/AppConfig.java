5235b582d4da13db3a2f37d44c4cb95246215430
kimbomm <qhals0710@gmail.com>
2026-02-24 10:48:30 +0900

    feature: spring 컨테너에 등록

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixedDiscountPolicy;
 import hello.core.member.MemberService;
@@ -9,19 +12,25 @@ import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
+
+  @Bean
   public MemberRepository memberRepository() {
     return new MemoryMemberRepository();
   }
 
+  @Bean
   public MemberService memberService() {
     return new MemberServiceImpl(memberRepository());
   }
 
+  @Bean
   public DiscountPolicy discountPolicy() {
     return new FixedDiscountPolicy();
   }
 
+  @Bean
   public OrderService orderService() {
     return new OrderServiceImpl(memberRepository(), discountPolicy());
   }
